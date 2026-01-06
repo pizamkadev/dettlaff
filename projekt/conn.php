f26e77a148e728baf07ea932d49429c8768cb5bd
pizamkadev <pizamadev@gmail.com>
2026-01-06 19:53:55 +0100

    Delete projekt directory

--- projekt/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $conn = new mysqli('localhost', 'root', '', 'rezerwacje');
-
-    if(!$conn){
-        echo "BŁĄD!!!!".mysqli_error();
-    }
-?>
--- projekt/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include 'utils.php';
-    $id_rez = $_GET['id_rez'];
-    $res = selectById($id_rez);
-    if($_POST){
-        delete($id_rez);
-        Header("Location: index.php");
-    }
-?>
